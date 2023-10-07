d9c38d92093b1de4ba2b7d9f78429b30aeb86f64
Breno Camarô <111977366+BrenoCamaro@users.noreply.github.com>
2023-10-07 07:21:20 -0300

    Update pilha.c

--- atividade7/pilha.c
+++ atividade7/pilha.c
@@ -7,23 +7,23 @@ Pilha* criar_pilha() {
 }
 
 Pilha* incluir(Pilha* pilha, char novo_dado) {
-    Pilha* novo_item = (Pilha*) malloc(sizeof(Pilha));
-    novo_item->dado = novo_dado;
-    novo_item->proximo = pilha;
-    return novo_item;
+    Pilha* novo_item = (Pilha*) malloc(sizeof(Pilha)); // Aloca memória para um novo item na pilha
+    novo_item->dado = novo_dado; // Atribui o novo dado ao campo 'dado' do novo item
+    novo_item->proximo = pilha; // Atribui o ponteiro para o topo da pilha ao campo 'proximo' do novo item
+    return novo_item;  // Retorna um ponteiro para o novo item, que agora é o novo topo da pilha
 }
 
 Pilha* remover(Pilha* pilha) {
-    if (pilha != NULL) {
-        Pilha* temp = pilha->proximo;
-        free(pilha);
-        return temp;
+    if (pilha != NULL) { // verifica se a pilha não está vazia
+        Pilha* temp = pilha->proximo; // cria um ponteiro temporário para o próximo elemento na pilha
+        free(pilha); // Libera a memória alocada para o elemento no topo da pilha
+        return temp; //retorna um ponteiro para um novo topo da pilha
     }
     return NULL;
 }
 
 int esta_vazia(Pilha* pilha) {
-    return (pilha == NULL);
+    return (pilha == NULL); //Retorna 1 se a pilha estiver vazia e 0 caso contrário
 }
 
 int esta_balanceado(char* expressao) {
